c3771ef7385d5d7ee998c69ae01302890e1c34aa
root <root@DESKTOP-QLFAEBA.localdomain>
2023-01-09 21:40:43 +0200

    homework part 3(fixed)

--- hw3.cpp
+++ hw3.cpp
@@ -3,42 +3,40 @@ using namespace std;
 
 int main()
 {
-for (int i=1; i<=5; i++)
-{
-    for(int j=1;j<=5-i;j++)
+    for (int i = 1; i <= 5; i++)
     {
-        cout << " ";
-    }
-   
-   for(int m=1;m<=2*i-1;m++)
-    {
-        cout << "X";
-    }
-   
-   for(int n=1; n<=2*(5-i);n++)
-    {
-        cout << " ";
-    }
-    
-    for(int k=1;k<=2*i;k++)
-    {
-        cout << "X";
-    }
-     cout << endl;
+        for (int j = 1; j <= 5 - i; j++)
+        {
+            cout << " ";
+        }
 
-    
-}
+        for (int m = 1; m <= 2 * i - 1; m++)
+        {
+            cout << "X";
+        }
 
-for (int i=2*5-1;i>=1;i--)
-{
-    for(int j=1; j<=2*5-1-i;j++)
-    {
-        cout << " ";
+        for (int n = 1; n <= 2 * (5 - i); n++)
+        {
+            cout << " ";
+        }
+
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            cout << "X";
+        }
+        cout << endl;
     }
-    for(int m=1; m<=2*i;m++)
+
+    for (int i = 2 * 5 - 1; i >= 1; i--)
     {
-        cout << "X";
+        for (int j = 1; j <= 2 * 5 - 1 - i; j++)
+        {
+            cout << " ";
+        }
+        for (int m = 1; m <= 2 * i; m++)
+        {
+            cout << "X";
+        }
+        cout << endl;
     }
-    cout << endl;
-}
 }
